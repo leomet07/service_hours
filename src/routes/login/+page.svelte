1e5d1e686671b440094d7b906554c4b669596ee4
leomet07 <leomet07@gmail.com>
2023-06-24 23:45:41 -0400

    Styles for login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,19 +3,56 @@
 	import { pb } from "$lib/pocketbase";
 </script>
 
-<form
-	method="POST"
-	use:enhance={() => {
-		return async ({ result }) => {
-			pb.authStore.loadFromCookie(document.cookie);
-			await applyAction(result);
-		};
-	}}
->
+<main>
 	<h1>Log in</h1>
-	<div>
+	<form
+		method="POST"
+		use:enhance={() => {
+			return async ({ result }) => {
+				pb.authStore.loadFromCookie(document.cookie);
+				await applyAction(result);
+			};
+		}}
+	>
+		<label for="email">Enter your email:</label>
 		<input type="email" name="email" placeholder="email@stuy.edu" />
-		<input type="password" name="password" placeholder="Password" />
+		<br />
+		<label for="email">Enter your password:</label>
+		<input
+			type="password"
+			name="password"
+			placeholder="a_secure_password_or_passphrase"
+		/>
+		<br />
 		<button>Log in</button>
-	</div>
-</form>
+	</form>
+</main>
+
+<style scoped>
+	main {
+		text-align: center;
+		display: block;
+	}
+
+	h1 {
+		font-size: 3rem;
+	}
+
+	form {
+		max-width: 400px;
+		margin-top: 10px;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	form input {
+		margin-top: 2px;
+		width: 100%;
+	}
+
+	form label {
+		font-size: 1.4rem;
+		font-weight: bold;
+		text-align: left;
+	}
+</style>
