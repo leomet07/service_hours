8107e8e8cf00e229123fbc0f2279a3d1281d3c2f
leomet07 <leomet07@gmail.com>
2023-06-25 00:03:26 -0400

    Style the about page and add github link

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,27 @@
 <main>
-	<h1>This is a pretty cool about page, right?</h1>
+	<h1>About</h1>
+	<h2>This is a pretty cool about page, right?</h2>
+	<p>
+		View the <a href="https://github.com/leomet07/service_hours">
+			source code
+		</a> for this project.
+	</p>
 </main>
+
+<style scoped>
+	main {
+		text-align: center;
+	}
+	h1 {
+		text-align: center;
+		font-size: 3rem;
+	}
+
+	a {
+		text-decoration: underline;
+	}
+
+	p {
+		margin-top: 10px;
+	}
+</style>
